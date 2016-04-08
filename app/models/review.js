6e6c0aaf9a7189810f55072e5113e401be045baf
Jordan Meier <jbirdmeier@gmail.com>
2016-04-08 10:13:01 -0700

    add a date property to review model

--- app/models/review.js
+++ app/models/review.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
+  date: DS.attr('date', {defaultValue() {return new Date(); } }),
   rating: DS.attr('number'),
   comment: DS.attr(),
   hike: DS.belongsTo('hike', {async: true})
