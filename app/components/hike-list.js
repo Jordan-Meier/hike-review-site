ae42eb1df0919863651318253f718b7b787e61b8
Jordan Meier <jbirdmeier@gmail.com>
2016-04-08 13:26:26 -0700

    delete unneccessary commented out code

--- app/components/hike-list.js
+++ app/components/hike-list.js
@@ -23,37 +23,3 @@ export default Ember.Component.extend({
     }
   },
 });
-
-
-// hikes: Ember.computed('filterByDifficulty', function() {
-//   var hikes = this.get('model');
-//   var output = [];
-//   if (this.get('filterByDifficulty') === "all") {
-//     return hikes;
-//   } else if (this.get("filterByDifficulty") === "easy") {
-//     hikes.forEach(function (hike) {
-//       if (hike.get('difficulty') <= 3) {
-//         output.push(hike);
-//       }
-//     });
-//   } else if (this.get("filterByDifficulty") === "medium") {
-//     hikes.forEach(function (hike) {
-//       if (hike.get(('difficulty') > 3 && <= 5) {
-//         output.push(hike);
-//       }
-//     });
-//   } else if (this.get("filterByDifficulty") === "difficult") {
-//     hikes.forEach(function (hike) {
-//       if (hike.get('difficulty') > 5 && <= 8) {
-//         output.push(hike);
-//       }
-//     });
-//   } else {
-//     hikes.forEach(function (hike) {
-//       if (hike.get('difficulty') > 8 && <= 10) {
-//         output.push(hike);
-//       }
-//     });
-//     return output;
-//   }
-// }),
