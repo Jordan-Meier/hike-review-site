4b47f33060e135e461ed023faa1d1a56c9da0e25
Jordan Meier <jbirdmeier@gmail.com>
2016-04-08 08:04:52 -0700

    install emberfire and bootstrap and add firebase url to environment.js config file

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'hike-review',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://hike-review.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
