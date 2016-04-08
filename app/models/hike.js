6bd475e79b3c66ff5f4b53d0031062edcb609406
Jordan Meier <jbirdmeier@gmail.com>
2016-04-08 08:20:20 -0700

    add a description property to hike model

--- app/models/hike.js
+++ app/models/hike.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   name: DS.attr(),
   location: DS.attr(),
   image: DS.attr(),
+  description: DS.attr(),
   difficulty: DS.attr('number'),
   reviews: DS.hasMany('review', {async: true})
 });
